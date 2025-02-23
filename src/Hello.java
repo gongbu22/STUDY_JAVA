d32920d343daf71e8fc545c4f9149fd7537d8de5
gongbu22 <pyujin0711@naver.com>
2025-02-24 00:02:12 +0900

    scanner

--- src/Hello.java
+++ src/Hello.java
@@ -1,9 +1,20 @@
-
+// 주석
 public class Hello {
+	
+	public static int sum(int n, int m) {
+		return n + m;
+	}
 
 	public static void main(String[] args) {
-		System.out.println("Hello Java");
-
+		int i = 20;
+		int s;
+		char a;
+		
+		s = sum(i, 10);
+		a = '?';
+		System.out.println(a);
+		System.out.println("Hello");
+		System.out.println(s);
 	}
 
 }
